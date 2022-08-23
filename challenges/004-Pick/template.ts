ac1469c5b1ee2b1f5ec36c265bfac205679c4683
dong <CheneyDong123@163.com>
2022-08-23 23:20:06 +0800

    feat: update

--- challenges/004-Pick/template.ts
+++ challenges/004-Pick/template.ts
@@ -16,12 +16,12 @@ function myPick(todo, keys) {
 
 // 要点提取
 // 1. 返回一个对象
-// 2. 遍历 keys
-// - 使用 Mapped type--in 操作符对 keys 进行遍历
+// 2. 遍历 keys  关键字：Mapped
+// - 使用 in 操作符对 keys 进行遍历
 //   - https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
 // 3. 去除不在 todo 中的 key 
-// - 使用 keyof 对类型提取
+// - 使用 keyof 对类型提取  关键字：lookup
 //   - https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
-// - 再使用 extends 来约束类型   Generic Constraints
+// - 再使用 extends 来约束类型  关键字：Generic Constraints
 //   -https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
 // 4. 将 key 赋值给 picked
