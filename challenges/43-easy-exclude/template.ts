370f71e09355a7cc817bcbf9cf7305b6a3823cf3
dong <CheneyDong123@163.com>
2022-08-29 10:59:37 +0800

    feat: done 43

--- challenges/43-easy-exclude/template.ts
+++ challenges/43-easy-exclude/template.ts
@@ -1,2 +1,10 @@
 // type MyExclude<T, U> = any
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T;
+
+function exclude(T, U) {
+  return T.filter((item) => !U.includes(item));
+}
+
+// 总结
+// 1. 当 extends 两边为一个 union 时，会执行类似于双重 for 循环的效果
+// 2. never 代表为不存在这个类型，即排除这个类型
