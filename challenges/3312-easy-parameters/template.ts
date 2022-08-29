dcf0e1c0983bb61176a5cdf51ba6486136ccf142
dong <CheneyDong123@163.com>
2022-08-29 22:43:43 +0800

    feat: done

--- challenges/3312-easy-parameters/template.ts
+++ challenges/3312-easy-parameters/template.ts
@@ -1,1 +1,5 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer P) => any ? P : never;
+
+const foo = (arg1: string, arg2: number): void => {}
+
+type a = typeof foo
