6f930d09ccc3f9ed2c01d835834799ed336864eb
dong <CheneyDong123@163.com>
2022-08-30 14:53:51 +0800

    feat: update

--- challenges/3-medium-omit/template.ts
+++ challenges/3-medium-omit/template.ts
@@ -1,2 +1,16 @@
 // type MyOmit<T, K> = any
-type MyOmit<T, K> = any
+type MyOmit<T, K extends keyof T> = {
+  [P in Exclude<keyof T, K>]: T[P]
+}
+
+// js
+function omit() {
+
+}
+
+// 思路
+// 1. 返回一个对象
+// 2. 遍历对象 T
+// 3. 去除 K 中的元素
+// 4. 如果 T 中的元素不在 K 中，则添加到返回对象中
+// 5. K 中元素必须是 T 中的元素，否则报错
