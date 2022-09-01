6d39bdfb433c50e3b258ed4f3a68a3780d8d2de4
dong <CheneyDong123@163.com>
2022-09-01 12:56:41 +0800

    feat: update

--- challenges/16-medium-pop/template.ts
+++ challenges/16-medium-pop/template.ts
@@ -1,1 +1,13 @@
+// pop
 type Pop<T extends any[]> = T extends [...infer Rest, infer Last] ? Rest : never
+// shift
+type Shift<T extends any[]> = T extends [infer First, ...infer Rest] ? Rest : never
+// push
+type MyPush<T extends any[] ,U> = [...T, U]
+// unshift
+type MyUnshift<T extends any[], U> = [U, ...T]
+
+// 总结
+// 1. infer 
+// 2. rest parameters
+// 3. ... spread operator
--- challenges/16-medium-pop/test.cases.ts
+++ challenges/16-medium-pop/test.cases.ts
@@ -3,4 +3,13 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
   Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
+
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<['a', 'b', 'c', 'd']>, ['b', 'c', 'd']>>,
+
+  Expect<Equal<MyPush<[3, 2], 1>, [3, 2, 1]>>,
+  Expect<Equal<MyPush<['a', 'b', 'c'], 'd'>, ['a', 'b', 'c', 'd']>>,
+
+  Expect<Equal<MyUnshift<[2, 1], 3>, [3, 2, 1]>>,
+  Expect<Equal<MyUnshift<['b', 'c', 'd'], 'a'>, ['a', 'b', 'c', 'd']>>,
 ]
