80e196b0305eaf82cb08b6cc12e150c72ecd3b57
dong <CheneyDong123@163.com>
2022-09-04 16:03:50 +0800

    fix: improve

--- challenges/9-medium-deep-readonly/template.ts
+++ challenges/9-medium-deep-readonly/template.ts
@@ -8,5 +8,6 @@ type DeepReadonly<T> = {
 };
 
 // 总结
-// 1. 在进行条件判断时，要注意 Function 也是 Object 的子类型，所以要使用 extends Function 来先判断去除 Function 类型
+// 1. 在进行条件判断时，要注意 Function 也是 Object 的子类型，
+// 所以要使用 extends Function 来先判断去除 Function 类型
 // 判断完之后，在进行递归调用
