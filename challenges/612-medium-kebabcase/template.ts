e61f7c8e7c46488e5cfdce668c6319ad95b97e69
dong <CheneyDong123@163.com>
2022-09-05 14:52:09 +0800

    feat: done 612-kebabcase

---
+++ challenges/612-medium-kebabcase/template.ts
@@ -0,0 +1,8 @@
+type KebabCase<S extends string> = S extends `${infer F}${infer R}`
+  ? R extends Uncapitalize<R>
+    ? `${Lowercase<F>}${KebabCase<R>}`
+    : `${Lowercase<F>}-${KebabCase<R>}`
+  : S;
+
+
+  type ass= KebabCase<'FooBarBaz'>
