8503d485d2034f5a8857e2a142a57ebc173d1f48
dong <CheneyDong123@163.com>
2022-09-06 15:08:59 +0800

    feat: update

--- challenges/949-medium-anyof/template.ts
+++ challenges/949-medium-anyof/template.ts
@@ -1,4 +1,6 @@
-type FalseSpace = "" | 0 | false | [] | { [p:string]: never };
+// type FalseSpace = "" | 0 | false | [] | { [p:string]: never };
+type FalseSpace = "" | 0 | false | [] | Record<PropertyKey, never>;
+// 以上二者等同
 
 // 方式一，使用 infer 以及递归来解决问题
 // type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
@@ -16,8 +18,11 @@ type add = AnyOf<[{}]>;
 // 总结
 // 1. 在判断 object 是否为空的时候不可以直接使用 extends 来进行判断
 // 需要判断他的键值是否为空,即 { [p:string]: never }
-// 或是 Record<string, never>
-// - https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type
+// 或是 Record<PropertyKey, never>
+//   ·Record<keys, value>
+//   - https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type
+//   ·PropertyKey 
+//   - https://www.typescriptlang.org/docs/handbook/2/mapped-types.html#handbook-content
 // 2. 如果是一个 tuple 可以使用 T[number] 来将他转换为 union
 // - https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
 // 3. 使用 Array<union> 来将 union 转化为 tuple，也可以创建一个 Array
