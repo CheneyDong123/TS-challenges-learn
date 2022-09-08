7529adb2242c12c9f5f4ea3ffbfc0a181f9a52cc
dong <CheneyDong123@163.com>
2022-09-08 22:07:25 +0800

    feat: done 2688-startwith

---
+++ challenges/2688-medium-startswith/test.cases.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<StartsWith<'abac', 'ac'>, false>>,
+  Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
+  Expect<Equal<StartsWith<'abc', 'abcd'>, false>>,
+  Expect<Equal<StartsWith<'abc', ''>, true>>,
+  Expect<Equal<StartsWith<'abc', ' '>, false>>,
+]
