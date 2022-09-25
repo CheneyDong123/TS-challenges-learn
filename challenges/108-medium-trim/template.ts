bf15ed18d328c37acebbe5588ed76cb30161dbb5
dong <CheneyDong123@163.com>
2022-09-25 10:58:01 +0800

    fix: update

--- challenges/108-medium-trim/template.ts
+++ challenges/108-medium-trim/template.ts
@@ -3,4 +3,4 @@ type workSplace2 = " " | "\n" | "\t";
 type Trim<S extends string> = S extends `${infer R}${workSplace2}` ? Trim<R> : TrimLeft<S>;
 
 // 总结
-// 先判断是否有左左边有空格，有则先去除左边的空格，再去除右边的空格
+// 先判断是否右边有空格，有则先去除右边的空格，再去除左边的空格
