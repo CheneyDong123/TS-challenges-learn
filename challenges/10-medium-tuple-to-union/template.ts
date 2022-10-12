c26f0aadccb278e6074a021243d323be0e7dd4d1
dong <CheneyDong123@163.com>
2022-10-12 11:13:06 +0800

    fix: update solution

--- challenges/10-medium-tuple-to-union/template.ts
+++ challenges/10-medium-tuple-to-union/template.ts
@@ -1,8 +1,8 @@
 // type TupleToUnion<T> = any
 // 解法一
-type TupleToUnion<T> = T extends [infer First, ...infer Rest] ? First | TupleToUnion<Rest>: never
+// type TupleToUnion<T> = T extends [infer First, ...infer Rest] ? First | TupleToUnion<Rest>: never
 // 解法二
-// type TupleToUnion<T extends any[]> = T extends T ? T[number] : never
+type TupleToUnion<T extends any[]> = T[number]
 
 // 总结
 // 1. 利用 infer 来推断 tuple 内的元素，之后使用递归来进行编写
